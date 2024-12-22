e67ba3668ddd37558372ed7570241e4706d0b656
omnia emad <omniaemad063@gmail.com>
2024-12-22 20:13:12 +0200

    testing commit

---
+++ lib/first_page.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:vetcare_project/login_page.dart';
+
+class MyFirstPage extends StatefulWidget {
+  const MyFirstPage({super.key});
+
+  @override
+  State<MyFirstPage> createState() => _MyFirstPageState();
+}
+
+class _MyFirstPageState extends State<MyFirstPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 2),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/background.png"))),
+          )
+        ],
+      ),
+    );
+  }
+}
